3ad3d3bc1dfd1267685e465ab3c6ee0d0411ce10
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-01-30 08:24:19 -0800

    text bubble scaling

---
+++ Assets/TextBubScaler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextBubScaler : MonoBehaviour {
+	public RectTransform bubText;
+
+	void Start () {
+		bubText = transform.parent.transform.Find("TalkBubText").gameObject.GetComponent<RectTransform>();
+	}
+	
+	void Update () {
+		GetComponent<RectTransform>().sizeDelta = 
+			new Vector2(
+				GetComponent<RectTransform>().sizeDelta.x, 
+				bubText.sizeDelta.y * 1.1f
+			);
+
+
+//		bubText.anchoredPosition = new Vector2(GetComponent<RectTransform>().anchoredPosition.x, GetComponent<RectTransform>().anchoredPosition.y); 
+	
+	}
+}
